53cbde2d5b595b36257b9714808ad38037ccd3f2
tim <tjhickey@brandeis.edu>
2016-07-13 07:18:03 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+This is the readme file for your git repository
+It can use the markdown syntax to make it more interesting visually.
---
+++ server/startup.js
@@ -0,0 +1,4 @@
+Meteor.startup(function(){
+	// put code here that you need to initialize your app
+	// the first time you run it, e.g. creating fake data for testing
+});
